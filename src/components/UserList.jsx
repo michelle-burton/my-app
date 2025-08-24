4d369606d235a0dcd01429a22ae0c4f77bea9d26
Michelle Burton <msmichelleburton@yahoo.com>
2025-08-24 15:01:45 -0500

    skelton site

---
+++ src/components/UserList.jsx
@@ -0,0 +1,13 @@
+function UserList({ users }) {
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          <strong>{user.name}</strong> — {user.email}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default UserList;
